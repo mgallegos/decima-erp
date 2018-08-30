eb369bd63d7a899ff25f85a0d51cdcf5c5beab10
Mario Gallegos <mgallegos76@gmail.com>
2018-08-30 14:43:41 -0600

    Issue #1

--- resources/views/layouts/header-javascript-cdn-b4.blade.php
+++ resources/views/layouts/header-javascript-cdn-b4.blade.php
@@ -46,7 +46,7 @@
 
 <script src="{{ URL::asset('assets/kwaai/js/helpers-v1.0.0.js') }}"></script>
 <script src="{{ URL::asset('assets/kwaai/js/apps-engine-v1.0.2.js') }}"></script>
-<script src="{{ URL::asset('assets/jquery-mg-validation-v0.3/jquery.jqMgVal.src.js') }}"></script>
+<script src="{{ URL::asset('assets/jquery-mg-validation-v0.4/jquery.jqMgVal.src.js') }}"></script>
 <script src="{{ URL::asset('assets/kwaai/js/base-v1.0.5.js') }}"></script>
 
 <script type='text/javascript'>
