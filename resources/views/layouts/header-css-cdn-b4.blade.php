756e066fea43c162d0bc8e50f4b88aa4c8ac9511
Mario Gallegos <mgallegos76@gmail.com>
2018-11-27 13:54:17 -0600

    Issue #1

--- resources/views/layouts/header-css-cdn-b4.blade.php
+++ resources/views/layouts/header-css-cdn-b4.blade.php
@@ -22,5 +22,5 @@
 		<link rel="stylesheet" href="{{ trim(preg_replace('/\s+/', ' ', $css)) }}" />
 	@endforeach
 @endif
-{!! Html::style('assets/kwaai/css/main-v1.0.8.css') !!}
+{!! Html::style('assets/kwaai/css/main-v1.0.9.css') !!}
 {!! Html::style('assets/kwaai/css/button-custom-classes.css') !!}
