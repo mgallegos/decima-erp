e79cac0a246ddd0b148d0aa614d64af631495212
Mario Gallegos <mgallegos76@gmail.com>
2019-03-16 13:07:50 -0600

    Issue #1

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('container')
 {!! Html::script('assets/kwaai/js/dashboard.js') !!}
 <div class="row">
-	{!! Form::journals('da-', $appInfo['id'], true, '', '', true, Lang::get('dashboard.journalLegend'), $organizationJournals) !!}
+	
 </div>
 @parent
 @stop
