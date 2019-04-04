67a41a3c1aa8d96ef26172d5a18f2ced217571bd
Mario Gallegos <mgallegos76@gmail.com>
2019-04-04 17:34:53 -0600

    Issue #1

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
 {!! Html::script('assets/kwaai/js/dashboard.js') !!}
 <div class="row">
 	<div class="col-md-6 col-md-offset-3">
-		<div class="alert alert-info" role="alert"> <strong>¡Nuevo menú!</strong> Haz clic el icono <i class="fa fa-bars core-menu-top-bar-link-color"></i> ubicado en el esquina superior derecha de la pantalla para acceder al menú del sistema.</div>
+		<div class="alert alert-info" role="alert"> <strong>¡Nuevo menú!</strong> Haz clic en el icono <i class="fa fa-bars core-menu-top-bar-link-color"></i> ubicado en el esquina superior derecha de la pantalla o el atajo ctrl + shift + m para acceder al menú del sistema.</div>
 	</div>
 </div>
 @parent
