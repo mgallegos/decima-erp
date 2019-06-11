17244434c7d4b45d2f81a550c7570f4203bdd772
Mario Gallegos <mgallegos76@gmail.com>
2019-06-11 16:54:07 -0600

    Issue #1

--- resources/lang/es/form.php
+++ resources/lang/es/form.php
@@ -279,5 +279,7 @@ return array(
 
 	'Y' => 'Generada',
 
+	'Z' => 'Procesada',
+
 	'statusGridText' => ':;P:Pendiente;U:Autorizado;A:Anulado;M:Masculino;F:Femenino;S:Soltero;C:Casado;V:Viudo;O:Aprobada;R:Rechazada;B:Abierta;T:Activo;I:Inactivo;L:Con licencia;D:Borrador;E:Publicado;H:Oculto;N:Enviada;J:Valor Fijo;K:Porcentaje;X:Pagado;Y:Generada',
 );
