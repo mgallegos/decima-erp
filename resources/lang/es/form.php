82ee56c859281b26662547555e0f03d67f9a84d6
Mario Gallegos <mgallegos76@gmail.com>
2020-07-14 16:51:14 -0600

    Issue #1

--- resources/lang/es/form.php
+++ resources/lang/es/form.php
@@ -8,310 +8,157 @@
  */
 
 return array(
-
 	'autocompleteValidation' => 'Debe elegir una de las opciones disponibles por el autocompletado de texto.',
-
 	'charactersAvailable' => 'Caracteres disponibles:',
-
 	'fieldRequiredHelpText' => '* Campo obligatorio',
-
 	'fieldRequired' => 'El campo es requerido.',
-
 	'radioRequired' => 'Debe seleccionar una opción.',
-
 	'checkboxRequired' => 'Debe elegir al menos una de las opciones.',
-
 	'passwordRequired' => 'Si el usuario no será activado por correo electrónico, es necesario que defina una contraseña.',
-
 	'dateRangeFrom' => 'Desde:',
-
 	'dateRangeTo' => 'Hasta:',
-
 	'from' => 'Del',
-
 	'to' => 'Al',
-
 	'on' => 'sobre',
-
 	'dateRangeFromPlaceHolder' => 'Desde',
-
 	'dateRangeToPlaceHolder' => 'Hasta',
-
 	'dateRangeValidation' => 'La fecha inicial no puede ser mayor a la fecha final.',
-
 	'defaultinvalidDateHelpMessage' => 'La fecha es inválida.',
-
 	'invalidDateFromHelpMessage' => 'La fecha inicial es inválida.',
-
 	'invaliddateToHelpMessage' => 'La fecha final es inválida.',
-	
 	'invalidParameters' => 'Parámetros inválidos.',
-
 	'month' => 'Mes',
-	
 	'months' => 'Meses',
-
 	'of' => 'de',
-
 	'monthOf' => 'Mes de :month',
-
 	'year' => 'Año',
-	
 	'years' => 'Años',
-
 	'gridFooterPageNumber' => ':page de :totalPages',
-
 	'gridFooterPagingInfo' => 'Mostrando :from - :to de :records',
-
 	'positiveIntegerRegexHelpMessage' => 'El valor ingresado debe ser un entero positivo, ejemplo: 1,000 o 1000',
-
 	'positiveIntegerNoZeroRegexHelpMessage' => 'El valor ingresado debe ser un entero positivo (no esta permitido cero), example: 1,000 o 1000',
-
 	'signedIntegerRegexHelpMessage' => 'El valor ingresado debe ser un entero, ejemplo: -1 o 1 o 1,000 o 1000',
-
 	'moneyRegexHelpMessage' => 'El valor ingresado debe ser númerico, ejemplo: 1,000.00 o 1000.00 o 1000',
-
 	'multipleDefaultSuccessOperationMessage' => 'Se realizaron las siguientes operaciones exitosamente:',
-
 	'defaultSuccessOperationMessage' => 'La operación se realizó exitosamente.',
-
 	'defaultSuccessSaveMessage' => 'El registro se guardó exitosamente.',
-
 	'defaultSuccessUpdateMessage' => 'El registro se actualizó exitosamente.',
-
 	'defaultSuccessDeleteMessage' => 'El registro se eliminó exitosamente.',
-
 	'defaultSuccessDeleteMessage1' => 'Los registros eliminaron exitosamente.',
-
 	'defaultAuthorizeMessage' => 'El registro se autorizó exitosamente.',
-
 	'defaultVoidMessage' => 'El registro se anuló exitosamente.',
-
 	'defaultAlreadyAuthorizeMessage' => 'El registro ya se encuentra autorizado.',
-
 	'defaultAlreadyVoidMessage' => 'El registro ya se encuentra anulado.',
-
 	'defaultErrorMessage' => 'Su petición no ha podido ser procesada, por favor intente de nuevo más tarde.',
-
 	'tokenMismatchException' => 'El token de seguridad de su sesión ha expirado, por seguridad se procederá a refrescar la ventana de su navegador.',
-
 	'authenticationException' => 'Su sesión ha expirado, por seguridad se procederá a refrescar la ventana de su navegador para que pueda iniciar sesión nuevamente.',
-
 	'formatIdentifierValidation' => 'El elemento no tiene asociado un formato de impresión',
-
 	'filtersValidation' => 'Debe completar al menos un filtro.',
-
 	'databaseConnectionNameInfoMessage' => 'Una vez creada la organización, el nombre de la conexión a la base de datos puede ser modificado únicamente por el administrador de la aplicación.',
-
 	'booleanText' => ':;0:No;1:Sí',
-
 	'loadButton' => 'Cargando...',
-
 	'no' => 'No',
-
 	'yes' => 'Sí',
-
 	'filtersTitle' => 'Filtros Disponibles',
-
 	'filterButton' => 'Aplicar filtros',
-
 	'clearFilterButton' => 'Limpiar filtros',
-
 	'thousandsSeparator' => ',',
-
 	'decimalSeparator' => '.',
-
 	'dateFormat' => 'dd/mm/yy',
-
 	// 'dateTimeFormat' => 'dd/mm/yy hh:mm:ss',
-
 	'dateTimePlaceHolder' => 'dd/mm/yy 12:00 a.m.',
-
 	'phpShortDateFormat' => 'd/m/Y',
-
 	'phpDateFormat' => 'd/m/Y h:i:s A',
-
 	'phpDateTimeFormat' => 'd/m/Y h:i a',
-	
 	'phpDateTime24hourFormat' => 'd/m/Y H:i',
-
 	'jsDateFormat' => 'DD/MM/YYYY',
-
 	'jsDateTimeFormat' => 'DD/MM/YYYY hh:mm a',
-
 	'mysqlDateFormat' => '%d/%m/%Y',
-
 	'defaultNumericValue' => '0.00',
-
 	'invalidSelection' => 'Debe seleccionar un registro antes de realizar esta operación.',
-
 	'emptyRows' => 'No se encontraron registros.',
-
 	'previous' => 'Anterior',
-
 	'next' => 'Siguiente',
-
 	'skip' => 'Omitir',
-
 	'done' => 'Finalizar',
-
 	'uploadImage' => 'Clic aquí para cambiar la imagen',
-
 	'name' => 'Nombre',
-
 	'deleteMessageConfirmation' => '¿Está seguro que desea eliminar el(los) registro(s) seleccionado(s)?',
-
 	'status' => 'Estado',
-
 	'statusPlural' => 'Estado',
-
 	'type' => 'Tipo',
-
 	'typeHelperText' => 'Filtrar por uno o más tipos',
-
 	'price' => 'Precio',
-
 	'total' => 'Total',
-
 	'totals' => 'Totales',
-
 	'date' => 'Fecha',
-
 	'dates' => 'Fechas',
-
 	'dateTime' => 'Fecha y hora',
-
 	'numeric' => 'Númerico',
-
 	'numerics' => 'Númericos',
-
 	'madeBy' => 'Hecho por',
-
 	'revisedBy' => 'Revisado por',
-
 	'authorizedBy' => 'Autorizado por',
-
 	'signature' => 'F.',
-
 	'number' => 'No.',
-
 	'page' => 'Página',
-
 	'orderBy' => 'Ordenar por',
-
 	'search' => 'Buscar',
-
 	'hours' => 'Horas',
-
 	'enterComment' => 'Escriba su comentario',
-
 	'access' => 'Acceder',
-
 	'generate' => 'Generar',
-
 	'generatePdf' => 'Generar PDF',
-
 	'generic' => 'Genérico',
-
 	'custom' => 'Personalizado',
-
 	'correlativeShort' => 'Correlat.',
-
 	'correlative' => 'Correlativo',
-
 	'reference' => 'Referencia',
-
 	'remark' => 'Observación',
-
 	'concept' => 'Concepto',
-
 	'spreadsheet' => 'Hoja de Cálculo',
-
 	'abbreviation' => 'Abreviatura',
-
 	'sums' => 'Sumas',
-
 	'son' => 'Son',
-
 	'letter' => 'Tamaño carta',
-
 	'legal' => 'Tamaño oficio',
-
 	'in' => 'En',
-
 	'syncMan' => 'Sincronizado',
-
 	'syncFem' => 'Sincronizada',
-
 	'active' => 'Activo',
-
 	'dearmf' => 'Estimado/a',
-
 	'user' => 'Usuario',
-
 	'greeting' => 'Un saludo',
-
 	'gender' => 'Género',
-
 	'code' => 'Código',
-
 	'userCustomFormat' => 'Formato de usuario',
-
 	'devCustomFormat' => 'Formato personalizado',
-
 	'file' => 'Archivo',
-
 	'searchModalTitle' => 'Seleccione el registro que desea editar',
-
 	'tryAgain' => 'Volver a intentar',
-
   'A' => 'Anulado',
-
 	'B' => 'Abierta',
-
 	'C' => 'Casado',
-
 	'D' => 'Borrador',
-
 	'E' => 'Publicado',
-
 	'F' => 'Mujer',
-
 	'G' => 'Entregada',
-
 	'H' => 'Oculto',
-
 	'I' => 'Inactivo',
-
 	'J' => 'Valor Fijo',
-
 	'K' => 'Porcentaje',
-
 	'L' => 'Con licencia',
-
 	'M' => 'Hombre',
-
 	'N' => 'Enviada',
-
 	'O' => 'Aprobada',
-
 	'P' => 'Pendiente',
-
 	'R' => 'Rechazada',
-
   'S' => 'Soltero',
-
 	'T' => 'Activo',
-
 	'U' => 'Autorizado',
-
   'V' => 'Viudo',
-
 	'X' => 'Pagado',
-
 	'Y' => 'Generada',
-
 	'Z' => 'Procesada',
-
 	'statusGridText' => ':;P:Pendiente;U:Autorizado;A:Anulado;M:Masculino;F:Femenino;S:Soltero;C:Casado;V:Viudo;O:Aprobada;R:Rechazada;B:Abierta;T:Activo;I:Inactivo;L:Con licencia;D:Borrador;E:Publicado;H:Oculto;N:Enviada;J:Valor Fijo;K:Porcentaje;X:Pagado;Y:Generada',
 );
