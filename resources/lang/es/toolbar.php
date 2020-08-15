9d6fd08cf3771c4d144e091f14959a16a279a056
Mario Gallegos <mgallegos76@gmail.com>
2020-08-15 12:40:38 -0600

    Issue #1

--- resources/lang/es/toolbar.php
+++ resources/lang/es/toolbar.php
@@ -8,71 +8,37 @@
  */
 
 return array(
-
 	'new' => 'Nuevo',
-
 	'view' => 'Ver',
-
 	'viewLongText' => 'Ver detalles del registro',
-
 	'edit' => 'Editar',
-
 	'save' => 'Guardar',
-
 	'delete' => 'Eliminar',
-
 	'remove' => 'Remover',
-
 	'nulify' => 'Anular',
-
 	'authorize' => 'Autorizar',
-
 	'process' => 'Procesar',
-
 	'upload' => 'Adjuntar',
-
 	'uploadLongText' => 'Adjuntar archivo',
-
 	'add' => 'Agregar',
-
 	'open' => 'Abrir',
-
 	'close' => 'Regresar',
-
 	'closeAlt' => 'Cerrar',
-
 	'refresh' => 'Actualizar',
-
 	'refreshLongText' => 'Actualizar la tabla de datos',
-
 	'changeStatus' => 'Cambiar estado',
-
 	'showFiles' => 'Ver archivos',
-
 	'showFilesLongText' => 'Ver archivos asociados',
-
 	'showHistoryLongText' => 'Ver historial',
-
 	'export' => 'Exportar',
-
 	'import' => 'Importar',
-
 	'copy' => 'Copiar',
-
 	'cancel' => 'Cancelar',
-
 	'print' => 'Imprimir',
-
 	'select' => 'Seleccionar',
-
 	'search' => 'Buscar',
-
 	'exportLongText' => 'Exportar datos de la tabla',
-
 	'closeLongText' => 'Cerrar el registro actual y volver a la vista de tabla (se perderán los cambios que no hayan sido guardados)',//'Close the current record and return to grid view',
-
 	'desc' => 'Ordenar los registros de forma descendente',
-
 	'asc' => 'Ordenar los registros de forma ascendente',
-
 );
