8bfb9dc53b466364f4c8aba87a24795b18b86650
Mario Gallegos <mgallegos76@gmail.com>
2021-02-13 06:15:59 -0600

    Issue #1

--- resources/lang/es/toolbar.php
+++ resources/lang/es/toolbar.php
@@ -37,6 +37,7 @@ return array(
 	'print' => 'Imprimir',
 	'select' => 'Seleccionar',
 	'search' => 'Buscar',
+	'generate' => 'Generar',
 	'exportLongText' => 'Exportar datos de la tabla',
 	'closeLongText' => 'Cerrar el registro actual y volver a la vista de tabla (se perderán los cambios que no hayan sido guardados)',//'Close the current record and return to grid view',
 	'desc' => 'Ordenar los registros de forma descendente',
