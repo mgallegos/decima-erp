a0d24e31620d74a7e6a5d83a1a302e9c636946d1
Mario Gallegos <mgallegos76@gmail.com>
2021-07-15 22:06:06 -0600

    Issue #1

--- app/Kwaai/System/Services/Validation/AbstractLaravelValidator.php
+++ app/Kwaai/System/Services/Validation/AbstractLaravelValidator.php
@@ -10,6 +10,7 @@
 namespace App\Kwaai\System\Services\Validation;
 
 use Illuminate\Validation\Factory;
+use Illuminate\Support\Str;
 
 abstract class AbstractLaravelValidator implements ValidableInterface {
 
@@ -152,6 +153,8 @@ abstract class AbstractLaravelValidator implements ValidableInterface {
 	  // $this->commit($openTransaction);
 	  // $this->rollBack($openTransaction);
 
+		$this->currentAttempt = 3;
+
 		if(empty($databaseConnectionName))
     {
       $this->databaseConnectionName = $this->AuthenticationManager->getCurrentUserOrganizationConnection();
@@ -213,6 +216,40 @@ abstract class AbstractLaravelValidator implements ValidableInterface {
     {
       $this->DB->connection($this->databaseConnectionName)->rollBack();
     }
+	}
+
+	/**
+	 * Was exception caused by a deadlock
+	 *
+	 * @param boolean $openTransaction
+	 *
+	 * @return array
+	 */
+	function causedByDeadlock($e)
+	{
+		$message = $e->getMessage();
+    
+		$causedByDeadlock =  Str::contains($message, [
+			'Deadlock found when trying to get lock',
+			'deadlock detected',
+			'The database file is locked',
+			'database is locked',
+			'database table is locked',
+			'A table in the database is locked',
+			'has been chosen as the deadlock victim',
+			'Lock wait timeout exceeded; try restarting transaction',
+			'WSREP detected deadlock/conflict and aborted the transaction. Try restarting the transaction',
+		]);
+
+		if ($causedByDeadlock($e) && $this->currentAttempt > 1)
+		{
+			$this->currentAttempt--;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 }
